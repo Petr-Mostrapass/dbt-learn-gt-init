b7bf5f1ea9624ff9fc687b40eb869b1b5a718c2e
jenna bushspies <jenna.bushspies+1764701251@dbtlabsworkshop.com>
2025-12-02 19:13:13 +0000

    added full name to customers model

--- models/customers.sql
+++ models/customers.sql
@@ -43,6 +43,7 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        concat(customers.first_name , ' ', customers.last_name) as full_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
