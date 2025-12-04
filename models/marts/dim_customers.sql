182a3a0dabdcd296cfd575036f2536606c7772ae
chaiyot pornruttanapun <chaiyot.pornruttanapun@dbtlabsworkshop.com>
2025-12-04 14:28:42 +0000

    commit

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,28 @@
+{{ config(
+    materialized="table",
+    tags="finance"
+) }}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id, 
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -25,7 +30,6 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
@@ -48,7 +52,6 @@ final as (
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
-
     left join customer_orders using (customer_id)
 
 )
